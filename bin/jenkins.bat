9cfd1b3f600b80dc58316b0a881d2737e7e892ec
yus2 <yus2@cisco.com>
2018-01-17 22:56:52 +0800

    update NuGet package version.

--- bin/jenkins.bat
+++ bin/jenkins.bat
@@ -9,7 +9,7 @@ pushd .
 del sdk\SparkSDKTests\app.config
 copy c:\app.config sdk\SparkSDKTests\app.config
 REM .\bin\nuget.exe update -self
-bin\nuget.exe restore sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln
+bin\nuget.exe restore sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln -NonInteractive
 "%MSBUILDDIR%\msbuild.exe" sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln /t:Rebuild /p:Configuration="Debug" /p:Platform="x86"
 "%MSBUILDDIR%\msbuild.exe" sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln /t:Rebuild /p:Configuration="Release" /p:Platform="x86"
 popd
