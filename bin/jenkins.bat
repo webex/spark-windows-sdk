22642adf61553d0cd672ec2f7d76dcc9a3ae5aff
liahan <liahan@cisco.com>
2018-06-11 14:57:06 +0800

    auto generate nuget

--- bin/jenkins.bat
+++ bin/jenkins.bat
@@ -1,9 +1,6 @@
 
 REM @echo OFF
 
-echo "copy scf libraries"
-copy spark-client-framework\scfLibrary\Release\*.dll sdk\binary\[build]\bin\x86\Release\
-
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin" set MSBUILDDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin
 echo %MSBUILDDIR%
 
@@ -12,6 +9,10 @@ del sdk\SparkSDKTests\app.config
 copy c:\app.config sdk\SparkSDKTests\app.config
 REM .\bin\nuget.exe update -self
 bin\nuget.exe restore sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln -NonInteractive
+
+echo "copy scf libraries"
+xcopy spark-client-framework\scfLibrary\Release\*.dll sdk\solutions\WinSDK4Desktop\packages\Cisco.Spark.WindowsSDK.*\
+
 "%MSBUILDDIR%\msbuild.exe" sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln /t:Rebuild /p:Configuration="Debug" /p:Platform="x86"
 "%MSBUILDDIR%\msbuild.exe" sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln /t:Rebuild /p:Configuration="Release" /p:Platform="x86"
 popd
