f4abacc8e34624d0003af2d69a1a57e3f5b5f57e
liahan <liahan@cisco.com>
2018-06-12 16:50:15 +0800

    auto-building

--- bin/jenkins.bat
+++ bin/jenkins.bat
@@ -1,5 +1,5 @@
 
-REM @echo OFF
+@echo OFF
 
 if exist "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin" set MSBUILDDIR=%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin
 echo %MSBUILDDIR%
@@ -10,22 +10,28 @@ copy c:\app.config sdk\SparkSDKTests\app.config
 REM .\bin\nuget.exe update -self
 bin\nuget.exe restore sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln -NonInteractive
 
-echo "copy scf libraries"
+echo copy scf libraries
 
-set SDKNuGetPackage = Cisco.Spark.WindowsSDK.1.4.0-EFT01
-echo %SDKNuGetPackage%
-copy /y spark-client-framework\scfLibrary\Release\*.dll sdk\solutions\WinSDK4Desktop\packages\%SDKNuGetPackage%\
-
-if %errorlevel% == 0 (
-	echo "build release version success!"
-) else (
-	echo "build release version failed."
-	echo %errorlevel%
-	goto EXIT
+set SDKNuGetPackage=Cisco.Spark.WindowsSDK.1.4.0-EFT01
+echo SDKNuGetPackage is %SDKNuGetPackage%
+copy /y spark-client-framework\scfLibrary\Release\*.dll sdk\solutions\WinSDK4Desktop\packages\\%SDKNuGetPackage%\\
+if not %errorlevel% == 0 ( 
+	echo update scf libraries failed.
+	goto EXIT 
 )
 
 "%MSBUILDDIR%\msbuild.exe" sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln /t:Rebuild /p:Configuration="Debug" /p:Platform="x86"
+if not %errorlevel% == 0 ( 
+	echo build debug version failed!
+	goto EXIT 
+)
+
 "%MSBUILDDIR%\msbuild.exe" sdk\solutions\WinSDK4Desktop\WinSDK4Desktop.sln /t:Rebuild /p:Configuration="Release" /p:Platform="x86"
+if not %errorlevel% == 0 (
+	echo build release version failed!
+	goto EXIT
+)
+
 popd
 
 call bin\mstest.bat
@@ -33,5 +39,5 @@ call bin\mstest.bat
 call bin\packageNuGet.bat
 
 :EXIT
-echo %errorlevel% 
+echo error level: %errorlevel% 
 EXIT /B %errorlevel%
